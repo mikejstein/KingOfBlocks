5ad11264285268a5749e3f387cfe295efc3afaa9
mikejstein <mike.j.stein@gmail.com>
2016-08-24 15:07:22 -0700

    Early enivornment work
    Added houses, smoke while flying, messed with skybox

---
+++ Assets/Scripts/FlyScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyScript : MonoBehaviour {
+    public GameObject head;
+    public GameObject rig;
+    public ParticleSystem smoke;
+
+    private bool _isActive;
+    public bool isActive {
+        get { return _isActive; }
+        set
+        {
+            _isActive = value;
+            if (_isActive)
+            {
+                smoke.Play();
+                // DO SOMETHING HERE
+            } else
+            {
+                smoke.Stop();
+            }
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        isActive = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 headLocation = head.transform.position;
+        Vector3 rigLocation = head.transform.position;
+        gameObject.transform.position = new Vector3(headLocation.x, rigLocation.y - 2, headLocation.z);
+        if ((rigLocation.y > 0) && (!isActive))
+        {
+            Debug.Log("MAKE ACTGIVE");
+            isActive = true;
+        } else if ((rigLocation.y == 0) && (isActive))
+        {
+            Debug.Log("KILL");
+            isActive = false;
+        }
+	}
+}
