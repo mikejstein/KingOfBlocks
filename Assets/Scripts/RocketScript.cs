60127071ff4cbb03417d16788ad6c3f043bb5be3
mikejstein <mike.j.stein@gmail.com>
2016-09-08 11:18:57 -0700

    Prep for reset command

---
+++ Assets/Scripts/RocketScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartFire();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void StartFire()
+    {
+        gameObject.BroadcastMessage("startFire");
+    }
+
+    void StopFire()
+    {
+        gameObject.BroadcastMessage("stopFire");
+    }
+}
